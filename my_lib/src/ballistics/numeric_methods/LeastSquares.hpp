1164439c7735724801bda7458df1122b1fd93eaa
KirillGavrish <k_gavrish5413@mail.ru>
2025-06-12 16:34:13 +0300

    some changes

--- my_lib/src/ballistics/numeric_methods/LeastSquares.hpp
+++ my_lib/src/ballistics/numeric_methods/LeastSquares.hpp
@@ -11,44 +11,60 @@
 
 static constexpr double eps = std::numeric_limits<double>::epsilon();
 static constexpr double eps10 = std::numeric_limits<double>::epsilon() * 10;
+static constexpr double eps1000 = std::numeric_limits<double>::epsilon() * 1000;
+
+template <typename T>
+class Observation
+{
+    std::vector<T> observation;
+    std::vector<double> observationTime;
+public:
+    Observation(std::vector<T> const &observation_, std::vector<double> const &observationTime_)
+        : observation(observation_), observationTime(observationTime_)
+    {}
+
+    T obs(std::size_t const n) const { return observation[n]; }
+    T time(std::size_t const n) const { return time[n]; }
+};
 
 template <typename State, typename NumericMethod>
 class LeastSquares
 {
-    std::vector<State> observation;
-    std::vector<double> observationTime;
+    Observation<State> data;
     std::vector<Eigen::Matrix<double, State::size(), State::size()>> W;
     NumericMethod numericMethod;
     std::size_t maxIt;
 public:
-    [[nodiscard]] std::size_t N() const { return observation.size(); };
-    LeastSquares(std::vector<State> const &observation_,
-                 std::vector<Eigen::Matrix<double, State::size(), State::size()>> const &W_, std::size_t const maxIt_)
-        : observation(observation_), W(W_), maxIt(maxIt_)
+    [[nodiscard]] std::size_t N() const { return data.observation.size(); }
+
+    LeastSquares(Observation<State> const &data_, std::vector<Eigen::Matrix<double, State::size(), State::size()>> const &W_,
+                 std::size_t const maxIt_)
+        : data(data_), W(W_), maxIt(maxIt_)
     {}
 
     std::tuple<State, Eigen::Matrix<double, State::size(), State::size()>> calc() const
     {
-        for (std::size_t it = 0; it < maxIt; ++it) {
-            State deltaX;
-            State const deltaX0 = observation[0] * 10e-8;
-            std::vector<Eigen::Matrix<double, State::size(), State::size()>> A(N, Eigen::Matrix<double, State::size(),
-                                                                                State::size()>::Zeros());
-            std::vector<Eigen::Vector<double, State::size()>> b(N, Eigen::Vector<double, State::size()>::Zeros());
+        State X0 = data.obs(0);
+        for (std::size_t it = 0; it < maxIt; ++it)
+        {
+            State const deltaX0 = X0 * eps1000;
+            std::vector<Eigen::Matrix<double, State::size(), State::size()>> const A(N(),
+                                                                                     Eigen::Matrix<double, State::size(), State::size()>::Zeros());
+            std::vector<Eigen::Vector<double, State::size()>> const b(N(), Eigen::Vector<double, State::size()>::Zeros());
             std::vector<State> computed;
-            State X0 = observation[0];
             for (std::size_t k = 0; k < State::size(); ++k)
             {
-                X0[k] += deltaX0[k];
-                computed.push_back(X0);
+                State tmpX0 = X0;
+                tmpX0[k] += deltaX0[k];
+                computed.push_back(tmpX0);
                 for (std::size_t n = 0; n < N()-1; ++n)
                 {
-                    computed.push_back(numericMethod(computed[n], observationTime[n+1],
-                                       observationTime[n+1] - observationTime[n], NumericMethod::RHS));
+                    computed.push_back(numericMethod(computed[n], data.time(n+1), data.time(n+1) - data.time(n),
+                                       NumericMethod::RHS));
                     for (std::size_t j = 0; j < State::size(); ++j)
                     {
-                        A[n](k, j) = (computed[n][j] - observation[n][j]) / deltaX0[k];
-                        b[n][j] = observation[n][j] - computed[n][j];
+                        A[n](k, j) = (computed[n][j] - data.obs(n)[j]) / deltaX0[k];
+                        b[n][j] = data.obs(n)[j] - computed[n][j];
                     }
                 }
             }
@@ -76,8 +92,8 @@ public:
                 }
                 return tmp;
             };
-            Eigen::Matrix<double, State::size(), State::size()> const P = calcP();
-            deltaX = P * calcATWb();
+            auto const P = calcP();
+            State deltaX = P * calcATWb();
             X0 = X0 + deltaX;
             if (std::sqrt(RMS()) < eps10) {
                 return {X0, P};
